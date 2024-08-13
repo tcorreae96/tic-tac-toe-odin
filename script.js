f731ee5c14803e0b29169be6cb09a9a304a7e176
Tomas Correa <79479585+tcorreae96@users.noreply.github.com>
2024-08-13 16:50:39 -0400

    starting with the logic

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 function Gameboard() {
-	this.board = [];
+	const gameboard = ["", "", "","", "", "","", "", ""];
+	const player1 = Player("Tomas", "X");
+	const player2 = Player("Gonzalo", "O");
+
+	const getGameboard = () => gameboard;
+
+	return { gameboard, getGameboard };
+}
+
+function Player(name, marker) {
+
+	const getPlayer = () => name;
+
+	return { name, marker, getPlayer };
+}
+
+function Game() {
 
 }
 
-function Player(name) {
-	this.name = name;
-	
-}
+const gameboard = Gameboard();
